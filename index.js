2f886911f4756cad05e8bb20fcb2efcc314c09dd
pcsishun <pcsishun@pcsishuns-MacBook-Air.local>
2021-08-08 17:34:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const express = require('express');
+var express_1 = __importDefault(require("express"));
+var app = express_1.default(); // ไว้สำหรับสร้าง  app backend เป็น object
+// การเขียน path จำเป็นต้องมีองค์ประกอบอยู่ 2 อย่าง ณ บริเวณ callback คือ req res ต้องเรียงตามรายละเอียดตามนี้
+// res ต้องดึง หรือเก็บเป็น json 
+// การใช้ res นั้นจะขึ้นบน serve เสมอ 
+app.get('/', function (req, res) {
+    console.log('this is index page.'); // จะถูกแสดงที่หน้า termial
+    res.json({
+        message: "Hello world"
+    });
+});
+// สามารถใช้เขียน web ได้แต่ไป react เถอะ 
+app.get('/home', function (req, res) {
+    console.log("this is homepage");
+    console.log("Asdasdasd");
+    res.send('<h2>This is homepage</h2>');
+});
+app.get('/todos', function (req, res) {
+    console.log("this is todos page.");
+    res.json({
+        path: "todos"
+    });
+});
+// จากนั้นการยิงออกไปต้องระบุ port พร้อมกับ callback ว่าจะให้วิ่งไปที่ไหน (จริงๆจะใส่หรือไม่ก็ได้เป็น option)
+app.listen(3000, function () { return console.log('serve is running.'); });
---
+++ index.ts
@@ -0,0 +1,33 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express() // ไว้สำหรับสร้าง  app backend เป็น object
+
+// การเขียน path จำเป็นต้องมีองค์ประกอบอยู่ 2 อย่าง ณ บริเวณ callback คือ req res ต้องเรียงตามรายละเอียดตามนี้
+// res ต้องดึง หรือเก็บเป็น json 
+// การใช้ res นั้นจะขึ้นบน serve เสมอ 
+
+app.get('/', (req, res) => {
+    console.log('this is index page.') // จะถูกแสดงที่หน้า termial
+    res.json({
+        message: "Hello world"
+    })
+})
+
+// สามารถใช้เขียน web ได้แต่ไป react เถอะ 
+app.get('/home', (req, res) => {
+    console.log("this is homepage")
+    console.log("Asdasdasd")
+    res.send('<h2>This is homepage</h2>')
+})
+
+app.get('/todos', (req, res) => {
+    console.log("this is todos page.")
+    res.json({
+        path: "todos"
+    })
+})
+
+// จากนั้นการยิงออกไปต้องระบุ port พร้อมกับ callback ว่าจะให้วิ่งไปที่ไหน (จริงๆจะใส่หรือไม่ก็ได้เป็น option)
+app.listen(3000, ()=> console.log('serve is running.'))
+
